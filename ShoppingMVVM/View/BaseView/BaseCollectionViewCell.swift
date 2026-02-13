47b24d1db4d2f8b01c1273c60c4e9fff7c8c8bd9
a-jb97 <a_jb97@naver.com>
2026-02-13 16:45:49 +0900

    Feat : BaseCollectionViewCell 구현

---
+++ ShoppingMVVM/View/BaseView/BaseCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  BaseCollectionViewCell.swift
+//  ShoppingMVVM
+//
+//  Created by 전민돌 on 2/13/26.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell, ViewDesignProtocol {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        contentView.backgroundColor = .white
+    }
+}
