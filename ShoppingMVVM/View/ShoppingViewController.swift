97c2c74ac957b0f4c7e0cd3aff5b607a2cfe5ede
a-jb97 <a_jb97@naver.com>
2026-02-13 16:13:55 +0900

    프로젝트 초기 세팅

--- ShoppingMVVM/View/ShoppingViewController.swift
+++ ShoppingMVVM/View/ShoppingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ShoppingViewController.swift
 //  ShoppingMVVM
 //
 //  Created by 전민돌 on 2/13/26.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ShoppingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
