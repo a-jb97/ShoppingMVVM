417d1ec595daebdb465ad5f2d6c1aa508d434850
a-jb97 <a_jb97@naver.com>
2026-02-13 16:40:00 +0900

    Feat : Shopping 구조체 생성

---
+++ ShoppingMVVM/Model/Shopping.swift
@@ -0,0 +1,20 @@
+//
+//  Shopping.swift
+//  ShoppingMVVM
+//
+//  Created by 전민돌 on 2/13/26.
+//
+
+import Foundation
+
+struct Shopping: Decodable {
+    let total: Int
+    let items: [ShoppingDetail]
+}
+
+struct ShoppingDetail: Decodable {
+    let title: String
+    let image: String
+    let lprice: String
+    let mallName: String
+}
